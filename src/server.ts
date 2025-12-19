21394c05c653d2f0c7fde7425a9ba792170d84db
Wiwee <grimstoey@gmail.com>
2025-12-19 18:20:11 +0700

    add git

---
+++ src/server.ts
@@ -0,0 +1,95 @@
+import express, {Request, Response} from 'express'
+import { getAllEvents, getEventByCategory, getEventById, addEvent } from "./services/EventService";
+import type  Event from "./models/Event";
+
+
+const app = express()
+const port = 3000
+app.use(express.json())
+
+
+
+///////////
+app.get("/events", (req, res) => {
+     if (req.query.category) {
+     const category = req.query.category as string;
+    const filteredEvents = getEventByCategory(category);
+     res.json(filteredEvents);
+    } else {
+
+    res.json(getAllEvents());
+     }
+ });
+
+
+app.get("/events/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const event = getEventById(id);
+     if (event) {
+     res.json(event);
+     } else {
+    res.status(404);
+     }
+    });
+    
+    
+app.post("/events", (req, res) => {       
+     const newEvent: Event = req.body;    
+    addEvent(newEvent);
+     res.json(newEvent);
+ });
+
+
+
+
+//////////////////
+// app.get('/test', (req: Request, res: Response) => {
+//     const id = req.query.id;
+//     const output = `id: ${id}`;
+//     res.send(output);
+// })
+
+
+
+// app.get('/test', (req, res) => {
+//     let returnObj = {
+//         name: 'test',
+//         age: 20,
+//         address: 'Thai'
+//     }
+//     res.send(returnObj);
+// })
+// app.get("/events/:id", (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const event = events.find((event) => event.id === id);
+//     if (event) {
+//         res.json(event);
+//     } else {
+//         res.status(404).send("Event not found");
+//     }
+// });
+
+// app.get("/events", (req, res) => {
+//     if (req.query.category) {
+//         const category = req.query.category;
+//         const filteredEvents = events.filter((event) => event.category === category);
+//         res.json(filteredEvents);
+//     } else {
+//         res.json(events);
+//     }
+// });
+
+// app.post("/events", (req, res) => {
+//     const newEvent: Event = req.body;
+//     newEvent.id = events.length + 1;
+//     events.push(newEvent);
+//     res.json(newEvent);
+// });
+
+
+
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
+
